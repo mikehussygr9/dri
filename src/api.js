0fcb4ac0f9385bfbb43d2aa1b1742e0f947ce42a
cifergciferg@gmail.com <cifergciferg@gmail.com>
2021-10-28 15:10:58 +0530

    first commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const requestIp = require('request-ip');
-const iplocate = require("node-iplocate");
+const ipInfo = require("ipinfo");
 const router=express.Router();
 
 const app = express();
@@ -9,23 +9,20 @@ router.get('/',(req, res) =>{
 
     const clientIp = requestIp.getClientIp(req);
     //const geo = geoip.lookup(ipg);
-    const ipg=clientIp.toString();
-    iplocate(ipg).then(function(results) {
-        if (results.country_code==="IN")
+    const ipg="14.140.206.158";
+    console.log('widn')
+    ipInfo(ipg, (err, cLoc) => {
+        if (cLoc.country==="IN")
             {
-                res.redirect('https://www.dropbox.com/s/z9jktzd4rmca4yc/data.txt?dl=1');
+                res.redirect('https://www.google.com');
             }
             else
             {
-                res.redirect("https://www.google.com/");
-            }
-        console.log("Country: " + results.country + " (" + results.country_code + ")"); 
-        });
-    
+                res.redirect("https://www.dropbox.com/s/z9jktzd4rmca4yc/data.txt?dl=1");
+            }        
+        console.log(cLoc.country);
+    });
 
 });
 app.use('/.netlify/functions/api',router);
 module.exports.handler=serverless(app);
-
-
-
