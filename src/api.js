168e26d8569aeb04b7583ffd494334305f08120a
mikehussygr9 <46367869+mikehussygr9@users.noreply.github.com>
2021-10-31 17:52:46 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -7,22 +7,18 @@ const router=express.Router();
 const app = express();
 router.get('/',(req, res) =>{
 
-    const clientIp = requestIp.getClientIp(req);
-    const clientIp = clientIp.toString();
+    //const clientIp = requestIp.getClientIp(req);
     //const geo = geoip.lookup(ipg);
-    ipInfo(clientIp,(err, cLoc) => {
-        res.send(cLoc.country)
-        /*if (cLoc.country=="IN")
-            {   
-                res.send(cLoc.country);
-                //res.redirect('https://www.google.com');
+    ipInfo((err, cLoc) => {
+        if (cLoc.country==="IN")
+            {
+                res.redirect('https://www.google.com');
             }
         else
-            {   
-                res.send(cLoc.ip);
-                //res.redirect("https://www.dropbox.com/s/z9jktzd4rmca4yc/data.txt?dl=1");
-            }  */      
-        //console.log(cLoc.country);
+            {
+                res.redirect("https://www.dropbox.com/s/z9jktzd4rmca4yc/data.txt?dl=1");
+            }        
+        console.log(cLoc.country);
     });
 
 });
